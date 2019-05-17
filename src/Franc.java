efa435178be4d0fde087a35d3aace955da22ebfd
Agnes <Agnes@Agnes>
2019-05-17 10:58:41 -0300

    Quarto commit do roteiro.

--- src/Franc.java
+++ src/Franc.java
@@ -1,13 +1,11 @@
 class Franc extends Money {   
-   //private int amount;					
-   Franc(int amount) {      
-      this.amount= amount;
-    }					
+
+   Franc(int amount, String currency) {
+      super(amount, currency);
+   }
+     
     Money times(int multiplier)  {      
-       return new Franc(amount * multiplier);					
+       return Money.franc(amount * multiplier);					
     }   
-    //public boolean equals(Object object) {					
-    //   Franc franc = (Franc) object;      
-    //   return amount == franc.amount;					
-    // }					
+    
 }
